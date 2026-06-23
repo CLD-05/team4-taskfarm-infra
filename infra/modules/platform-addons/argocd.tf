4a76ec3aef792dbc617e69591aed69c00b01d584
chatnip <commcencer@gmail.com>
2026-06-23 14:05:37 +0900

    fix: webhook 문제 수정

--- infra/modules/platform-addons/argocd.tf
+++ infra/modules/platform-addons/argocd.tf
@@ -21,10 +21,6 @@ resource "helm_release" "argocd" {
         service = {
           type = "ClusterIP"
         }
-
-        # [참고] ingress=false 유지. ArgoCD UI는 dev/prod 모두 port-forward로 접근하는
-        #   전제. prod에서 UI를 외부 노출하려면 ingress(ALB)+ESO(admin 비밀번호)를
-        #   추가해야 하나, 운영 UI를 공개 노출하지 않는 게 보안상 안전해 현행 유지.
         ingress = {
           enabled = false
         }
@@ -32,13 +28,13 @@ resource "helm_release" "argocd" {
 
       configs = {
         params = {
-          # [참고] server.insecure=true는 ArgoCD 서버 자체 TLS를 끔.
-          #   ALB/Ingress에서 TLS를 종단하는 구성이면 맞는 설정. 단 port-forward로만
-          #   접근하는 현재는 내부 통신이라 무방. 추후 ingress로 외부 노출 시
-          #   TLS 종단 지점(ALB)에 인증서가 있는지 확인 필요.
           "server.insecure" = true
         }
       }
     })
   ]
+
+  # [WEBHOOK-FIX] ALB Controller가 Ready 된 뒤 설치
+  depends_on = [helm_release.alb_controller]
 }
+
