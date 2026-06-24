fa598c1f036bf403bb64bfcae04bc90731e6a5ec
chatnip <commcencer@gmail.com>
2026-06-24 13:10:10 +0900

    fix: fargate 포트 분리

--- infra/modules/platform-addons/metrics-server.tf
+++ infra/modules/platform-addons/metrics-server.tf
@@ -5,9 +5,34 @@ resource "helm_release" "metrics_server" {
   version    = var.chart_versions.metrics_server
   namespace  = "kube-system"
 
-  # Fargate(dev) kubelet 인증서가 127.0.0.1용이라 IP 검증 실패 → 검증 스킵
+  # defaultArgs 통째로 재정의 (secure-port를 10251로, Fargate kubelet 10250과 분리)
   set {
-    name  = "args[0]"
+    name  = "defaultArgs[0]"
+    value = "--cert-dir=/tmp"
+  }
+  set {
+    name  = "defaultArgs[1]"
+    value = "--kubelet-preferred-address-types=InternalIP\\,ExternalIP\\,Hostname"
+  }
+  set {
+    name  = "defaultArgs[2]"
+    value = "--kubelet-use-node-status-port"
+  }
+  set {
+    name  = "defaultArgs[3]"
+    value = "--metric-resolution=15s"
+  }
+  set {
+    name  = "defaultArgs[4]"
     value = "--kubelet-insecure-tls"
   }
+  set {
+    name  = "defaultArgs[5]"
+    value = var.env == "dev" ? "--secure-port=10251" : "--secure-port=10250"
+  }
+  set {
+    name  = "containerPort"
+    value = var.env == "dev" ? "10251" : "10250"
+  }
+
 }
